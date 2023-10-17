793d8547ef76b6cfc42a7e703095182f459cd50d
Sandra <sandra.lamire.sl@gmail.com>
2023-10-17 15:46:40 +0200

    Ternaire

---
+++ meaning.py
@@ -0,0 +1,11 @@
+meaning = 42
+print()
+
+if meaning > 10:
+    print("rigth on!")
+else:
+    print("Not today")
+
+
+print("if/else en ternaire")
+print("rigth on!") if meaning > 10 else print("Not today")
---
+++ welcome.py
@@ -0,0 +1,27 @@
+greetings = "Hello World !"
+print(greetings)
+
+
+
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     WELCOME!     *"
+
+    
+print()
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+print("24/5 sans arrondi :", 24/5)
+# 4.8 = division donne toujours un résultat avec 1 décimale
+print("24//5 avec arrondi inférieur = round(24//5) :", 24//5)
+# 4 = division en étages
+print("24//5 avec arrondi supérieur = round(24/5) :", round(24/5))
+# 5
+
+x = True
+print(not x)
+# False
